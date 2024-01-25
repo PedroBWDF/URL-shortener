c5cf05ef37b1f6d9a95c1403096e697b9a1aeb72
PedroBWDF <bwdf64587@gmail.com>
2024-01-25 15:27:33 +0800

    set node.js file system

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const fs = require("fs")
+const dataFilePath = 'public/jsons/data.json'
+fs.readFile(dataFilePath, function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log("Asynchronous read: " + data.toString());
+});
+
+app.use(express.static('public'))
+
+
 app.get('/', (req, res) => {
   res.send('hola')
 })
 
 //設定動態路由
-app.get(':/shortURL', (req, res) => {
+app.get('/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL
-  res.send(`Generating short URL for: ${shortUrl}`)
+  res.send(`Generating short URL for: ${shortURL}`)
 })
 
 app.listen(port, () => {
